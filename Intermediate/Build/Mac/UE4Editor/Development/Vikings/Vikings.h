a6ff5fb17ce688e29bdd0ce072e028c1f767fbd4
Vinicius A.M. Lobo <vinicius@trustsoftwares.com.br>
2015-09-17 17:43:12 -0300

    First Upload

---
+++ Intermediate/Build/Mac/UE4Editor/Development/Vikings/Vikings.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_VIKINGS_H__
+#define __AUTO_VIKINGS_H__
+//Last Write: 17/09/2015 19:57:35 +00:00
+#include "/Users/Programmer/Documents/Unreal Projects/Vikings/Source/Vikings/Vikings.h"
+#endif//__AUTO_VIKINGS_H__
---
+++ Source/Vikings/VikingsGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "VikingsGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VIKINGS_API AVikingsGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
